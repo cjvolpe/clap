84cb0bb116593eaaa58bd7deb1ffee33e9aed381
cjvolpe <cjvolpe@unc.edu>
2026-04-16 11:25:20 -0400

    setup as PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Clapp',
+        short_name: 'Clapp',
+        description: "UNC Chapel Hill's Very Own Mountain Project",
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './src/lib/resources/clapping-hands.svg',
+            sizes: '192x192',
+            type: 'image/svg'
+          },
+          {
+            src: './src/lib/resources/clapping-hands.svg',
+            sizes: '512x512',
+            type: 'image/svg'
+          }
+        ]
+      }
+    })
+  ]
+});
